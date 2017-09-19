1e3ef8bca8e82074c2ae5146036e66677b98f986
Jorge Hernandez <jnandez565@gmail.com>
2017-09-19 23:15:50 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
+import axios from 'axios';
+import urlFor from './helpers/urlFor';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote
+    });
+  }
+
+getNotes = () => {
+  axios.get(urlFor('notes'))
+  .then((res) => console.log(res.data) )
+  .catch((err) => console.log(err.response.data) );
+}
+
+  render() {
+    const { showNote } = this.state;
+
+    return (
+      <div className="App">
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/helpers/urlFor.js
@@ -0,0 +1,5 @@
+const urlFor = (endpoint) => {
+  return 'https://note-api-jh.herokuapp.com/' + endpoint;
+}
+
+export default urlFor;
